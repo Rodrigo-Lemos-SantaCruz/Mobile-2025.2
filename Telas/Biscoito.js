4557200abd0107d803620ff014a8e2a965691983
Rodrigo-Lemos-SantaCruz <rodrigo.lemos@unisantacruz.edu.br>
2025-08-26 20:17:56 -0300

    Aula 26 Ago

--- Telas/Biscoito.js
+++ Telas/Biscoito.js
@@ -1,9 +1,21 @@
-import {View, Text} from 'react-native'
+import {View, Text, Image, Button} from 'react-native'
+import Rodape from '../Componentes/Rodape'
 
 export default function Biscoito() {
+    var fraseInspiradora = 'Uma frase de chines'
     return (
         <View>
             <Text>Biscoito da Sorte</Text>
+            <Image source={require('../assets/biscoito.png')} />
+            <Text>{fraseInspiradora}</Text>
+            <Button
+                title='Ver Frase'
+                onPress={()=>console.log('Frase sorteada')}
+            />
+            <Rodape
+                img1={require('../assets/biscoito.png')}
+                img2={require('../assets/calculadora.png')}
+            />
         </View>
     )
 }
--- Telas/Calculadora.js
+++ Telas/Calculadora.js
@@ -1,25 +1,22 @@
 import {View, Text, TextInput, Button} from 'react-native'
+import Form from '../Componentes/Form'
+import Rodape from '../Componentes/Rodape'
 
 export default function Calculadora() {
     var nota = 8
     return (
         <View>
-            <Text>Nota da Prova</Text>
-            <TextInput
-                placeholder='Notas de 0 a 10'
-                onChangeText={(text)=>console.log(text)}
-            />
-            <Text>Nota do Trabalho</Text>
-            <TextInput
-                placeholder='Notas de 0 a 10'
-                onChangeText={(text)=>console.log(text)}
-            />
+            <Form texto='Nota da Prova: ' />
+            <Form texto='Nota do Trabalho: ' />
             <Button 
                 title='Calcular'
                 onPress={()=>console.log('Botão apertado')}
             />
             <Text>A sua nota final é {nota}</Text>
-            
+            <Rodape 
+                img1={require('../assets/biscoito.png')}
+                img2={require('../assets/calculadora.png')}    
+            />
         </View>
     )
 }
